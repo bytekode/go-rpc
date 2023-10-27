cfb75869515aef4e82610e2deb4f0ee8aa3ebd46
bytekode <busydroider@gmail.com>
2023-10-28 01:03:15 +0530

    Updated client to send multiple message in single session

--- rpc-client.go
+++ rpc-client.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"go-rpc/common"
 	"log"
 	"net/rpc"
+	"os"
 )
 
 func main() {
-	sendMessageViaRPC()
+	// sendMessageViaRPC()
+	scanner()
 }
 
-func sendMessageViaRPC() {
+func scanner() {
+	scanner := bufio.NewScanner(os.Stdin)
+	log.Println("Enter your message..")
+	for scanner.Scan() {
+		message := scanner.Text()
+		if message != "" {
+			sendMessageViaRPC(message)
+		} else {
+			log.Println("Please enter a valid message!")
+		}
+		log.Println("Enter your message..")
+	}
+}
+
+func sendMessageViaRPC(message string) {
 	client, err := rpc.Dial("tcp", "localhost:5001")
 	common.Check(err)
 
 	rpcPayload := common.RPCPayload{
 		Name: "Message",
-		Data: "Hello World",
+		Data: message,
 	}
 
 	var result string
